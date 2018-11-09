7ccb7fe6363fdd686db286d4fde1f89c07a660d1
Perchemastik <davidr_6@hotmail.com>
2018-11-09 12:47:28 -0800

    event handling

--- exercises/08/js/main.js
+++ exercises/08/js/main.js
@@ -7,3 +7,17 @@ for(i=1;i<=(all_p.length);i+=1){
 	}
 	all_p[i-1].innerHTML = "Number "+ i.toString();
 }
+
+function function_name () {
+	// body... 
+	// document.getElementsByTagName("h2")[0].innerHTML="waazzzaaa";
+	document.getElementsByTagName("h2")[0].classList.toggle("text-danger");
+}
+
+function myfunction() {
+	document.getElementsByClassName("formInputs")[0].style.color = "blue";
+}
+
+function myanotherfunction() {
+	document.getElementsByClassName("formInputs")[1].style.color = "orange";
+}
--- exercises/08/readme.txt
+++ exercises/08/readme.txt
@@ -1,3 +1,4 @@
 javascript
 loops
 break and continue
+handling events: onclick, ondblclick, onblur>> changed colors and text
